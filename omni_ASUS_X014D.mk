cbbda5de21bee2b36a5e34e1e294dc63c45b5751
Ilya Kushnir <ilya.displax@gmail.com>
2018-06-18 14:53:31 +0300

    Init

---
+++ omni_ASUS_X014D.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/asus/ASUS_X014D/ASUS_X014D.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ASUS_X014D
+PRODUCT_NAME := omni_ASUS_X014D
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X014D
+PRODUCT_MANUFACTURER := asus
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo omni_ASUS_X014D-userdebug
